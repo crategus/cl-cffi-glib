806ddd87995842571b1f23efc2276f565d98b955
Crategus <kaiser@crategus.com>
2023-05-05 12:54:23 +0200

    Updating the get-flags-items test

--- test/rtest-gobject-utils.lisp
+++ test/rtest-gobject-utils.lisp
@@ -26,28 +26,29 @@
              (gobject:get-g-enum-definition "GEmblemOrigin"))))
 
 (test get-flags-items
-  (is (equal '("G_APPLICATION_FLAGS_NONE" "G_APPLICATION_IS_SERVICE"
-               "G_APPLICATION_IS_LAUNCHER" "G_APPLICATION_HANDLES_OPEN"
-               "G_APPLICATION_HANDLES_COMMAND_LINE"
+  (is (equal '("G_APPLICATION_FLAGS_NONE" "G_APPLICATION_DEFAULT_FLAGS"
+               "G_APPLICATION_IS_SERVICE" "G_APPLICATION_IS_LAUNCHER"
+               "G_APPLICATION_HANDLES_OPEN" "G_APPLICATION_HANDLES_COMMAND_LINE"
                "G_APPLICATION_SEND_ENVIRONMENT" "G_APPLICATION_NON_UNIQUE"
                "G_APPLICATION_CAN_OVERRIDE_APP_ID"
                "G_APPLICATION_ALLOW_REPLACEMENT" "G_APPLICATION_REPLACE")
              (mapcar #'flags-item-name
                      (get-flags-items "GApplicationFlags"))))
-  (is (equal '(0 1 2 4 8 16 32 64 128 256)
+  (is (equal '(0 0 1 2 4 8 16 32 64 128 256)
              (mapcar #'flags-item-value
                      (get-flags-items "GApplicationFlags"))))
-  (is (equal '("flags-none" "is-service" "is-launcher" "handles-open"
-               "handles-command-line" "send-environment" "non-unique"
-               "can-override-app-id" "allow-replacement" "replace")
+  (is (equal '("flags-none" "default-flags" "is-service" "is-launcher"
+               "handles-open" "handles-command-line" "send-environment"
+               "non-unique" "can-override-app-id" "allow-replacement" "replace")
              (mapcar #'flags-item-nick
                      (get-flags-items "GApplicationFlags")))))
 
 (test get-g-flags-definition
  (is (equal '(DEFINE-G-FLAGS "GApplicationFlags"
-                              G-APPLICATION-FLAGS
+                             G-APPLICATION-FLAGS
                              (:EXPORT T)
                              (:FLAGS-NONE 0)
+                             (:DEFAULT-FLAGS 0)
                              (:IS-SERVICE 1)
                              (:IS-LAUNCHER 2)
                              (:HANDLES-OPEN 4)
@@ -59,4 +60,4 @@
                              (:REPLACE 256))
             (gobject:get-g-flags-definition "GApplicationFlags"))))
 
-;;; --- 2023-1-2 ---------------------------------------------------------------
+;;; --- 2023-5-5 ---------------------------------------------------------------
