20fa83c1813ef71499f85c784f409274b34ff94e
Crategus <kaiser@crategus.com>
2024-10-12 19:54:11 +0200

    Work on implementation, documentation, tests

--- test/rtest-gio-file.lisp
+++ test/rtest-gio-file.lisp
@@ -5,40 +5,6 @@
 
 ;;; --- Types and Values -------------------------------------------------------
 
-;;;     GFilesystemPreviewType
-
-(test g-filesystem-preview-type
-  ;; Check type
-  (is (g:type-is-enum "GFilesystemPreviewType"))
-  ;; Check type initializer
-  (is (eq (g:gtype "GFilesystemPreviewType")
-          (g:gtype (cffi:foreign-funcall "g_filesystem_preview_type_get_type"
-                                         :size))))
-  ;; Check registered symbol
-  (is (eq 'gio:filesystem-preview-type
-          (glib:symbol-for-gtype "GFilesystemPreviewType")))
-  ;; Check names
-  (is (equal '("G_FILESYSTEM_PREVIEW_TYPE_IF_ALWAYS"
-               "G_FILESYSTEM_PREVIEW_TYPE_IF_LOCAL"
-               "G_FILESYSTEM_PREVIEW_TYPE_NEVER")
-             (glib-test:list-enum-item-names "GFilesystemPreviewType")))
-  ;; Check values
-  (is (equal '(0 1 2)
-             (glib-test:list-enum-item-values "GFilesystemPreviewType")))
-  ;; Check nick names
-  (is (equal '("if-always" "if-local" "never")
-             (glib-test:list-enum-item-nicks "GFilesystemPreviewType")))
-  ;; Check enum definition
-  (is (equal '(GOBJECT:DEFINE-GENUM "GFilesystemPreviewType"
-                                    GIO:FILESYSTEM-PREVIEW-TYPE
-                                    (:EXPORT T
-                                     :TYPE-INITIALIZER
-                                     "g_filesystem_preview_type_get_type")
-                                    (:IF-ALWAYS 0)
-                                    (:IF-LOCAL 1)
-                                    (:NEVER 2))
-             (gobject:get-gtype-definition "GFilesystemPreviewType"))))
-
 ;;;     GFile
 
 (test g-file-interface
@@ -61,53 +27,49 @@
              (glib-test:list-signals "GFile")))
   ;; Check interface definition
   (is (equal '(GOBJECT:DEFINE-GINTERFACE "GFile" GIO:FILE
-                                         (:EXPORT T
-                                          :TYPE-INITIALIZER "g_file_get_type"))
+                       (:EXPORT T
+                        :TYPE-INITIALIZER "g_file_get_type"))
              (gobject:get-gtype-definition "GFile"))))
-
-;;;     GFileQueryInfoFlags
-;;;     GFileCreateFlags
-;;;     GFileCopyFlags
-;;;     GFileMonitorFlags
-;;;     GFileMeasureFlags
 
 ;;; --- Functions --------------------------------------------------------------
 
-;;;     GFileProgressCallback
-;;;     GFileReadMoreCallback
-;;;     GFileMeasureProgressCallback
-;;;
+;;;     g:file-as-namestring
+
+(test g-file-as-namestring
+  (let ((path (glib-sys:sys-path "test/rtest-gio-file.lisp"))
+        (file nil))
+
+    (is (cffi:pointerp (setf file
+                             (cffi:convert-to-foreign path
+                                                      'g:file-as-namestring))))
+
+    (is (string= "rtest-gio-file.lisp" (g:file-basename file)))
+    (is (string= (namestring path) (g:file-path file)))
+    (is (string= (namestring path) (g:file-get-parse-name file)))
+
+    (is (string= (namestring path)
+                 (cffi:convert-from-foreign file 'g:file-as-namestring)))
+    (is-false (cffi:convert-from-foreign nil 'g:file-as-namestring))
+    (is-false (cffi:convert-from-foreign (cffi:null-pointer)
+                                         'g:file-as-namestring))))
+
 ;;;     g_file_new_for_path
 
-#-windows
 (test g-file-new-for-path
-  (let ((file (g:file-new-for-path "/home/dieter/path.lisp")))
+  (let* ((path (glib-sys:sys-path "test/rtest-gio-file.lisp"))
+         (file (g:file-new-for-path path)))
     (is (typep file 'g:object))
-    (is (string= "path.lisp" (g:file-basename file)))
-    (is (string= "/home/dieter/path.lisp" (g:file-path file)))))
-
-#+windows
-(test g-file-new-for-path
-  (let ((file (g:file-new-for-path "/home/dieter/path.lisp")))
-    (is (typep file 'g:object))
-    (is (string= "path.lisp" (g:file-basename file)))
-    (is (string= "\\home\\dieter\\path.lisp" (g:file-path file)))))
+    (is (string= "rtest-gio-file.lisp" (g:file-basename file)))
+    (is (string= (namestring path) (g:file-path file)))))
 
 ;;;     g_file_new_for_uri
 
-#-windows
 (test file-new-for-uri
-  (let ((file (g:file-new-for-uri "http://crategus.com")))
+  (let* ((path "http://crategus.com")
+         (file (g:file-new-for-uri path)))
     (is (typep file 'g:object))
     (is-false (g:file-path file))
-    (is (string= "http://crategus.com" (g:file-uri file)))))
-
-#+windows
-(test g-file-new-for-uri
-  (let ((file (g:file-new-for-uri "http://crategus.com")))
-    (is (typep file 'g:object))
-    (is-false (g:file-path file))
-    (is (string= "http://crategus.com/" (g:file-uri file)))))
+    (is (string= path (g:file-uri file)))))
 
 ;;;     g_file_new_for_commandline_arg
 
@@ -119,8 +81,6 @@
 (test g-file-new-for-commandline-arg-and-cwd
   (is (typep (g:file-new-for-commandline-arg-and-cwd "commandline"
                                                      "directory") 'g:object)))
-
-;;;     g_file_new_tmp
 
 ;;;     g_file_parse_name
 
@@ -147,11 +107,6 @@
   (let ((file (g:file-parse-name "http://crategus.com")))
     (is-false (g:file-path file))
     (is (string= "http://crategus.com/" (g:file-uri file)))))
-
-;;;     g_file_new_build_filename
-;;;     g_file_dup
-;;;     g_file_hash
-;;;     g_file_equal
 
 ;;;     g_file_get_basename
 
@@ -184,8 +139,6 @@
 (test g-file-path.2
   (let ((file (g:file-parse-name "http://crategus.com")))
     (is-false (g:file-path file))))
-
-;;;     g_file_peek_path
 
 ;;;     g_file_get_uri
 
@@ -225,121 +178,4 @@
   (let ((file (g:file-parse-name "http://crategus.com")))
     (is (string= "http://crategus.com/" (g:file-get-parse-name file)))))
 
-;;;     g_file_get_parent
-;;;     g_file_has_parent
-;;;     g_file_get_child
-;;;     g_file_get_child_for_display_name
-;;;     g_file_has_prefix
-;;;     g_file_get_relative_path
-;;;     g_file_resolve_relative_path
-;;;     g_file_is_native
-;;;     g_file_has_uri_scheme
-;;;     g_file_get_uri_scheme
-;;;     g_file_read
-;;;     g_file_read_async
-;;;     g_file_read_finish
-;;;     g_file_append_to
-;;;     g_file_create
-;;;     g_file_replace
-;;;     g_file_append_to_async
-;;;     g_file_append_to_finish
-;;;     g_file_create_async
-;;;     g_file_create_finish
-;;;     g_file_replace_async
-;;;     g_file_replace_finish
-;;;     g_file_query_info
-;;;     g_file_query_info_async
-;;;     g_file_query_info_finish
-;;;     g_file_query_exists
-;;;     g_file_query_file_type
-;;;     g_file_query_filesystem_info
-;;;     g_file_query_filesystem_info_async
-;;;     g_file_query_filesystem_info_finish
-;;;     g_file_query_default_handler
-;;;     g_file_query_default_handler_async
-;;;     g_file_query_default_handler_finish
-;;;     g_file_measure_disk_usage
-;;;     g_file_measure_disk_usage_async
-;;;     g_file_measure_disk_usage_finish
-;;;     g_file_find_enclosing_mount
-;;;     g_file_find_enclosing_mount_async
-;;;     g_file_find_enclosing_mount_finish
-;;;     g_file_enumerate_children
-;;;     g_file_enumerate_children_async
-;;;     g_file_enumerate_children_finish
-;;;     g_file_set_display_name
-;;;     g_file_set_display_name_async
-;;;     g_file_set_display_name_finish
-;;;     g_file_delete
-;;;     g_file_delete_async
-;;;     g_file_delete_finish
-;;;     g_file_trash
-;;;     g_file_trash_async
-;;;     g_file_trash_finish
-;;;     g_file_copy
-;;;     g_file_copy_async
-;;;     g_file_copy_finish
-;;;     g_file_move
-;;;     g_file_make_directory
-;;;     g_file_make_directory_async
-;;;     g_file_make_directory_finish
-;;;     g_file_make_directory_with_parents
-;;;     g_file_make_symbolic_link
-;;;     g_file_query_settable_attributes
-;;;     g_file_query_writable_namespaces
-;;;     g_file_set_attribute
-;;;     g_file_set_attributes_from_info
-;;;     g_file_set_attributes_async
-;;;     g_file_set_attributes_finish
-;;;     g_file_set_attribute_string
-;;;     g_file_set_attribute_byte_string
-;;;     g_file_set_attribute_uint32
-;;;     g_file_set_attribute_int32
-;;;     g_file_set_attribute_uint64
-;;;     g_file_set_attribute_int64
-;;;     g_file_mount_mountable
-;;;     g_file_mount_mountable_finish
-;;;     g_file_unmount_mountable
-;;;     g_file_unmount_mountable_finish
-;;;     g_file_unmount_mountable_with_operation
-;;;     g_file_unmount_mountable_with_operation_finish
-;;;     g_file_eject_mountable
-;;;     g_file_eject_mountable_finish
-;;;     g_file_eject_mountable_with_operation
-;;;     g_file_eject_mountable_with_operation_finish
-;;;     g_file_start_mountable
-;;;     g_file_start_mountable_finish
-;;;     g_file_stop_mountable
-;;;     g_file_stop_mountable_finish
-;;;     g_file_poll_mountable
-;;;     g_file_poll_mountable_finish
-;;;     g_file_mount_enclosing_volume
-;;;     g_file_mount_enclosing_volume_finish
-;;;     g_file_monitor_directory
-;;;     g_file_monitor_file
-;;;     g_file_monitor
-;;;     g_file_load_bytes
-;;;     g_file_load_bytes_async
-;;;     g_file_load_bytes_finish
-;;;     g_file_load_contents
-;;;     g_file_load_contents_async
-;;;     g_file_load_contents_finish
-;;;     g_file_load_partial_contents_async
-;;;     g_file_load_partial_contents_finish
-;;;     g_file_replace_contents
-;;;     g_file_replace_contents_async
-;;;     g_file_replace_contents_bytes_async
-;;;     g_file_replace_contents_finish
-;;;     g_file_copy_attributes
-;;;     g_file_create_readwrite
-;;;     g_file_create_readwrite_async
-;;;     g_file_create_readwrite_finish
-;;;     g_file_open_readwrite
-;;;     g_file_open_readwrite_async
-;;;     g_file_open_readwrite_finish
-;;;     g_file_replace_readwrite
-;;;     g_file_replace_readwrite_async
-;;;     g_file_replace_readwrite_finish
-;;;     g_file_supports_thread_contexts
-
-;;; 2024-9-17
+;;; 2024-10-12
