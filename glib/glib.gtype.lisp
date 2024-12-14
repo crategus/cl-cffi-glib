898e3a5d5624a127c81826e606ba515f97d97f8a
Crategus <kaiser@crategus.com>
2024-12-14 18:55:44 +0100

    Rework storage of symbols for GTypes

--- glib/glib.gtype.lisp
+++ glib/glib.gtype.lisp
@@ -26,6 +26,17 @@
 
 ;;; ----------------------------------------------------------------------------
 
+(defvar *warn-unknown-gtype* t)
+
+(defun warn-unknown-gtype (name)
+  (when *warn-unknown-gtype*
+    (let ((msg (if (stringp name)
+                   name
+                   (format nil "~a" name))))
+      (warn "~a is not known to the GType system" msg))))
+
+;;; ----------------------------------------------------------------------------
+
 ;; We need two functions from the GObject library. These functions are later
 ;; implemented a second time for the GObject API, but do not use the :size
 ;; type, but the Lisp GTYPE type.
@@ -44,24 +55,79 @@
   name
   %id)
 
+;;; ----------------------------------------------------------------------------
+
+;; Global hash tables to store names and ids of foreign GTypes
+
+(defvar *gtype-lock* (bt:make-lock "gtype lock"))
+
+(let ((name-to-gtype-table (make-hash-table :test 'equal))
+      (id-to-gtype-table (make-hash-table)))
+
+  (defun name-to-gtype (name)
+    (gethash name name-to-gtype-table))
+  (defun (setf name-to-gtype) (value name)
+    (setf (gethash name name-to-gtype-table) value))
+  (defun id-to-gtype (id)
+    (gethash id id-to-gtype-table))
+  (defun (setf id-to-gtype) (value id)
+    (setf (gethash id id-to-gtype-table) value))
+
+  (defun invalidate-gtypes ()
+    (bt:with-lock-held (*gtype-lock*)
+      (clrhash id-to-gtype-table)
+      (iter (for (name gtype) in-hashtable name-to-gtype-table)
+            (setf (gtype-%id gtype) nil))))
+
+  ;; For debugging purposes
+  (defun get-name-to-gtypes ()
+    (iter (for (name gtype) in-hashtable name-to-gtype-table)
+          (collect name)))
+  (defun get-id-to-gtypes ()
+    (iter (for (id gtype) in-hashtable id-to-gtype-table)
+          (collect (list id (gtype-name gtype))))))
+
+(glib-init:at-finalize () (invalidate-gtypes))
+
+;;; ----------------------------------------------------------------------------
+
 #+liber-documentation
 (setf (liber:alias-for-symbol 'gtype)
       "Struct"
       (liber:symbol-documentation 'gtype)
- "@version{2024-6-11}
+ "@version{2024-12-8}
   @begin{short}
     The @symbol{g:gtype} structure represents the unique identifier of a
-    registered @code{GType} type.
+    registered foreign @code{GType} type on the Lisp side.
   @end{short}
-  In the C library a @code{GType} type is represented as an integer.
+  In the C library a @code{GType} type is represented as an integer. The
+  @class{g:type-t} type specifier automatically converts between the Lisp
+  @symbol{g:gtype} representation and the C integer.
   @see-constructor{g:gtype}
+  @begin[Examples]{dictionary}
+    Create a @symbol{g:gtype} instance from a @code{GType} name:
+    @begin{pre}
+(g:gtype \"GApplication\")
+=> #<GTYPE :name \"GApplication\" :id 94607290994400>
+(g:gtype-name *) => \"GApplication\"
+(g:gtype-id **) => 94607290994400
+    @end{pre}
+  @end{dictionary}
   @see-slot{g:gtype-name}
   @see-slot{g:gtype-id}
   @see-class{g:type-t}")
 
+(defmethod print-object ((instance gtype) stream)
+  (if *print-readably*
+      (call-next-method)
+      (print-unreadable-object (instance stream)
+        (format stream "GTYPE :name \"~A\" :id ~D"
+                       (gtype-name instance)
+                       (gtype-%id instance)))))
+
 #+liber-documentation
 (setf (documentation 'gtype-name 'function)
- "@version{2024-6-11}
+ "@version{2024-12-8}
   @argument[instance]{a @symbol{g:gtype} instance}
   @return{The string with the name of the @code{GType} type.}
   @begin{short}
@@ -78,62 +144,19 @@
   @see-symbol{g:gtype}
   @see-function{g:gtype-id}")
 
-(defmethod print-object ((instance gtype) stream)
-  (if *print-readably*
-      (call-next-method)
-      (print-unreadable-object (instance stream)
-        (format stream "GTYPE :name \"~A\" :id ~D"
-                       (gtype-name instance)
-                       (gtype-%id instance)))))
-
-;;; ----------------------------------------------------------------------------
-
-;; Global hash tables to store names and ids of foreign GTypes
-
-(defvar *gtype-lock* (bt:make-lock "gtype lock"))
-(defvar *name-to-gtype* (make-hash-table :test 'equal))
-(defvar *id-to-gtype* (make-hash-table))
-
-(defun invalidate-gtypes ()
-  (bt:with-lock-held (*gtype-lock*)
-    (clrhash *id-to-gtype*)
-    (iter (for (name gtype) in-hashtable *name-to-gtype*)
-          (setf (gtype-%id gtype) nil))))
-
-(defun get-name-to-gtypes ()
-  (iter (for (name gtype) in-hashtable *name-to-gtype*)
-        (collect name)))
-
-(defun get-id-to-gtypes ()
-  (iter (for (id gtype) in-hashtable *id-to-gtype*)
-        (collect (list id (gtype-name gtype)))))
-
-(glib-init:at-finalize () (invalidate-gtypes))
-
-;;; ----------------------------------------------------------------------------
-
-(defvar *warn-unknown-gtype* t)
-
-(defun warn-unknown-gtype (name)
-  (when *warn-unknown-gtype*
-    (let ((msg (if (stringp name)
-                   name
-                   (format nil "~a" name))))
-      (warn "~a is not known to the GType system" msg))))
-
 ;;; ----------------------------------------------------------------------------
 
 ;; GTYPE-ID replaces the accessor GTYPE-%ID
 
 (defun gtype-id (instance)
  #+liber-documentation
- "@version{2024-6-11}
+ "@version{2024-12-8}
   @argument[instance]{a @symbol{g:gtype} instance}
   @return{The integer for the unique identifier of the @code{GType} type.}
   @begin{short}
-    Returns the unique identifiert for the @code{GType} type.
+    Returns the unique identifier for the @code{GType} type.
   @end{short}
-  The function returns 0 for a @code{nil} value. This represents an invalid
+  The function returns 0 for a @code{nil} value, representing an invalid
   @code{GType} type.
   @begin[Examples]{dictionary}
     @begin{pre}
@@ -156,12 +179,12 @@
                  (warn-unknown-gtype (gtype-name instance))
                  ;; Store the valid ID
                  (setf (gtype-%id instance) id
-                       (gethash id *id-to-gtype*) instance))
+                       (id-to-gtype id) instance))
              id)))))
 
 ;;; ----------------------------------------------------------------------------
 
-;; Make a Lisp GTYPE representation from a NAME or an ID
+;; Make a Lisp GTYPE representation from a NAME or ID
 
 ;; TODO: We have changed the implementation so that we do not store unknown
 ;; types in the hash tables. Because we have only valid types in the hash
@@ -170,7 +193,7 @@
 (defun gtype-from-name (name)
   (when name
     (bt:with-lock-held (*gtype-lock*)
-      (let ((gtype (gethash name *name-to-gtype*)))
+      (let ((gtype (name-to-gtype name)))
         (when gtype
           (unless (gtype-%id gtype)
             (let ((id (%g-type-from-name name)))
@@ -179,7 +202,7 @@
                   (warn-unknown-gtype name)
                   ;; Store the valid ID
                   (setf (gtype-%id gtype) id
-                        (gethash id *id-to-gtype*) gtype))))
+                        (id-to-gtype id) gtype))))
           (return-from gtype-from-name gtype)))
       (let ((id (%g-type-from-name name)))
         (if (zerop id)
@@ -187,14 +210,14 @@
             (warn-unknown-gtype name)
             ;; Generate and store a GTYPE
             (let ((gtype (make-gtype :name (copy-seq name) :%id id)))
-              (setf (gethash id *id-to-gtype*) gtype
-                    (gethash name *name-to-gtype*) gtype)
+              (setf (id-to-gtype id) gtype)
+              (setf (name-to-gtype name) gtype)
               (return-from gtype-from-name gtype)))))))
 
 (defun gtype-from-id (id)
   (unless (zerop id)
     (bt:with-lock-held (*gtype-lock*)
-      (let ((gtype (gethash id *id-to-gtype*)))
+      (let ((gtype (id-to-gtype id)))
         (if gtype
             gtype
             (let (;; FIXME: This might cause a bug, because %g-type-name expects
@@ -206,14 +229,14 @@
               (unless name
                 (warn-unknown-gtype id)
                 (return-from gtype-from-id nil))
-              (let ((gtype (gethash name *name-to-gtype*)))
+              (let ((gtype (name-to-gtype name)))
                 (when gtype
-                  (setf (gtype-%id gtype) id
-                        (gethash id *id-to-gtype*) gtype)
+                  (setf (gtype-%id gtype) id)
+                  (setf (id-to-gtype id) gtype)
                   (return-from gtype-from-id gtype))
                 (let ((gtype (make-gtype :name name :%id id)))
-                  (setf (gethash id *id-to-gtype*) gtype
-                        (gethash name *name-to-gtype*) gtype)
+                  (setf (id-to-gtype id) gtype)
+                  (setf (name-to-gtype name) gtype)
                   (return-from gtype-from-id gtype)))))))))
 
 ;;; ----------------------------------------------------------------------------
@@ -238,7 +261,7 @@
 
 #+liber-documentation
 (setf (documentation 'gtype 'function)
- "@version{2024-6-11}
+ "@version{2024-12-8}
   @argument[thing]{a string, an integer, or an @symbol{g:gtype} instance}
   @return{The @symbol{g:gtype} instance for @arg{thing}.}
   @begin{short}
@@ -251,7 +274,6 @@
 (g:gtype 20) => #<GTYPE :name \"gboolean\" :id 20>
 (g:gtype \"gboolean\") => #<GTYPE :name \"gboolean\" :id 20>
 (g:gtype (g:gtype 20)) => #<GTYPE :name \"gboolean\" :id 20>
-
 (g:gtype \"unknown\")
 => WARNING: unknown is not known to the GType system
 => NIL
@@ -269,6 +291,30 @@
 (let ((symbol-for-gtype (make-hash-table :test 'equal)))
 
   (defun symbol-for-gtype (name-or-gtype)
+    #+liber-documentation
+    "@version{2024-12-8}
+     @argument[name-or-gtype]{a string or a @symbol{g:gtype} instance
+       representing a valid @code{GType} type}
+     @return{The Lisp symbol representing the @code{GType} type.}
+     @begin{short}
+       Returns the Lisp symbol for the given @arg{name-or-gtype} argument.
+     @end{short}
+
+     The Lisp symbol is used with the @code{make-instance} method to create
+     an instance for a @code{GType} type from the Lisp side.
+     @begin[Examples]{dictionary}
+       @begin{pre}
+(g:symbol-for-gtype \"GApplication\")
+=> GIO:APPLICATION
+=> T
+(g:symbol-for-gtype (g:gtype \"GSimpleAction\"))
+=> GIO:SiMPLE-ACTION
+=> T
+(make-instance *)
+=> #<GIO:SIMPLE-ACTION {1007481A53@}>
+       @end{pre}
+     @end{dictionary}
+     @see-symbol{g:gtype}"
     (let ((key (if (stringp name-or-gtype)
                    name-or-gtype
                    (gtype-name (gtype name-or-gtype)))))
@@ -280,9 +326,9 @@
                    (gtype-name (gtype name-or-gtype)))))
     (setf (gethash key symbol-for-gtype) symbol)))
 
-    ;; For debugging purposes
-    (defun list-symbol-for-gtype ()
-      (iter (for (key symbol) in-hashtable symbol-for-gtype)
-            (collect symbol))))
+  ;; For debugging purposes
+  (defun list-symbol-for-gtype ()
+    (iter (for (key symbol) in-hashtable symbol-for-gtype)
+          (collect symbol))))
 
 ;;; --- End of file glib.gtype.lisp --------------------------------------------
